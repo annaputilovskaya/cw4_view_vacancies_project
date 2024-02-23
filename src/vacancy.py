77ff254899b84b8c27a6d7f121090987fc8e7685
putilovskaya <putilovskayaa@mail.ru>
2024-02-23 20:13:13 +0300

    fix vacancy.py

--- src/vacancy.py
+++ src/vacancy.py
@@ -2,19 +2,26 @@ class Vacancy:
     """
     Класс для работы с вакансией
     """
-    def __init__(self):
-        self.title = None
-        self.url = None
-        self.salary = self.get_salary()
-        self.requirements = None
+    def __init__(self, title=None, url=None, salary=0, requirements=None, area=None, employer=None):
+        self.title = title
+        self.url = url
+        self.salary = salary
+        self.requirements = requirements
+        self.area = area
+        self.employer = employer
 
-    def get_salary(self) -> float:
+    def __str__(self):
         """
-        Получает информацию о зарплате
-        :return: Размер зарплаты. В случае, если зарплата не укзана, 0.
+        Отображает информацию о ваканси для пользователей
         """
+        return f'{self.title}: {self.salary}'
 
-        return self.salary
+    def __repr__(self):
+        """
+        Отображает информацию о вакансии для разработчика
+        """
+        return (f'{self.__class__.__name__}({self.title=}, {self.url=}, {self.salary=}, '
+                f'{self.requirements=}, {self.area=}, {self.employer=})')
 
     def __lt__(self, other) -> bool:
         """
@@ -27,3 +34,52 @@ class Vacancy:
         Проверяет, является ли зарпата по данной вакансии выше, чем по другой вакансии
         """
         return self.salary > other.salary
+
+    @property
+    def salary(self):
+        return self.__salary
+
+    @salary.setter
+    def salary(self, salary):
+        if isinstance(salary, dict):
+            currency = salary.get('currency')
+            if currency == 'RUR':
+                self.__salary = salary.get('from')
+            else:
+                self.__salary = Vacancy.convert_to_rub(currency, salary.get('from'))
+        elif isinstance(salary, (int, float)):
+            self.__salary = salary
+        else:
+            self.__salary = 0
+
+    @staticmethod
+    def convert_to_rub(currency, amount):
+        """
+        Переводит в рубли сумму в евро или долларах по заданному курсу
+        """
+        rates = {
+            'EUR': 100,
+            'USD': 90
+        }
+        rate = rates.get(currency)
+        if rate is None:
+            print('Нет информации о курсе валюты')
+            return 0
+        return amount * rate
+
+    @classmethod
+    def cast_to_object_list(cls, data) -> list:
+        """
+        Создает список объектов класса Vacancy по данным из JSON
+        """
+        all_vacancies = []
+        for item in data:
+            title = item.get('name')
+            url = item.get('url')
+            salary = item.get('salary')
+            requirements = item.get('snippet').get('requirement')
+            area = item.get('area').get('name')
+            employer = item.get('employer').get('name')
+            vacancy = Vacancy(title, url, salary, requirements, area, employer)
+            all_vacancies.append(vacancy)
+        return all_vacancies
