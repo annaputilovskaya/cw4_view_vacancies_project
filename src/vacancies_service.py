e03e2150a3c4f6c4e4dbdf6958f88ac7bfe853b8
putilovskaya <putilovskayaa@mail.ru>
2024-02-23 20:14:47 +0300

    fix vacancies_service.py

--- src/vacancies_service.py
+++ src/vacancies_service.py
@@ -1,15 +1,18 @@
+import json
 from abc import ABC, abstractmethod
+
+import requests
 
 
 class VacanciesService(ABC):
     """
     Абстрактный класс для работы с API сервиса с вакансиями
     """
+
     @abstractmethod
     def get_vacancies(self):
         """
         Получает данные о вакансиях с сайта
-        :return: json-файл
         """
         pass
 
@@ -18,12 +21,23 @@ class HHVacanciesService(VacanciesService):
     """
     Класс для работы с вакансиями платформы hh.ru
     """
-    def get_vacancies(self):
+    def __init__(self):
+        """
+        Создает экземпляр класса HHVacanciesService
+        """
+        self._url = 'https://api.hh.ru/vacancies'
+
+    def get_vacancies(self, text=''):
         """
         Получает данные о вакансиях с сайта
-        :return: json-файл
         """
-        pass
+        response = requests.get(self._url, params={'per_page': 100, 'text': text})
+        if response.status_code == 200:
+            return response.json()['items']
+        else:
+            print('Возникла ошибка при обращении к сайту hh.ru')
 
 
-
+# hhv = HHVacanciesService()
+# v = hhv.get_vacancies()
+# print(json.dumps(v, indent=2, ensure_ascii=False))
