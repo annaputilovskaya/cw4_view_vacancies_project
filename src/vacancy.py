01b7c3f97c50199efb2d62a66996b08762103c43
putilovskaya <putilovskayaa@mail.ru>
2024-02-24 17:54:18 +0300

    fix vacancy.py

--- src/vacancy.py
+++ src/vacancy.py
@@ -5,6 +5,9 @@ class Vacancy:
     all = []
 
     def __init__(self, title=None, url=None, salary=0, requirements=None, area=None, employer=None):
+        """
+        Создает объект класса Vacancy
+        """
         self.title = title
         self.url = url
         self.salary = salary
@@ -12,30 +15,73 @@ class Vacancy:
         self.area = area
         self.employer = employer
 
-    def __str__(self):
+    def __str__(self) -> str:
         """
         Отображает информацию о ваканси для пользователей
         """
         return f'{self.title}: {self.salary}'
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         """
         Отображает информацию о вакансии для разработчика
         """
         return (f'{self.__class__.__name__}({self.title}, {self.url}, {self.salary}, '
                 f'{self.requirements}, {self.area}, {self.employer})')
 
-    def __lt__(self, other) -> bool:
+    def __ge__(self, other) -> bool:
         """
-        Проверяет, является ли зарпата по данной вакансии ниже, чем по другой вакансии
+        Проверяет, является ли зарпата по данной вакансии выше или равна другой
         """
-        return self.salary < other.salary
+        if isinstance(other, self.__class__):
+            return self.salary >= other.salary
+        elif isinstance(other, int):
+            return self.salary >= other
+        else:
+            raise TypeError("Сравнение данных объектов невозможно")
 
     def __gt__(self, other) -> bool:
         """
-        Проверяет, является ли зарпата по данной вакансии выше, чем по другой вакансии
+        Проверяет, является ли зарпата по данной вакансии выше другой
         """
-        return self.salary > other.salary
+        if isinstance(other, self.__class__):
+            return self.salary > other.salary
+        elif isinstance(other, int):
+            return self.salary > other
+        else:
+            raise TypeError("Сравнение данных объектов невозможно")
+
+    def __le__(self, other) -> bool:
+        """
+        Проверяет, является ли зарпата по данной вакансии ниже или равна другой
+        """
+        if isinstance(other, self.__class__):
+            return self.salary <= other.salary
+        elif isinstance(other, int):
+            return self.salary <= other
+        else:
+            raise TypeError("Сравнение данных объектов невозможно")
+
+    def __lt__(self, other) -> bool:
+        """
+        Проверяет, является ли зарпата по данной вакансии ниже другой
+        """
+        if isinstance(other, self.__class__):
+            return self.salary < other.salary
+        elif isinstance(other, int):
+            return self.salary < other
+        else:
+            raise TypeError("Сравнение данных объектов невозможно")
+
+    def __contains__(self, words):
+        """
+        Проверяет, содержит ли вакансия ключевые слова
+        """
+        flag = False
+        for word in words:
+            if word in self.requirements:
+                flag = True
+                break
+        return flag
 
     @property
     def salary(self):
@@ -43,13 +89,19 @@ class Vacancy:
 
     @salary.setter
     def salary(self, salary):
+        """
+        Устанавливает значение зарплаты.
+        Если в вакансии не указан размер зарплаты или
+        неизвестен курс перевода валюты, зарплата устанавливается  равной 0.
+        """
         if isinstance(salary, dict):
             currency = salary.get('currency')
             if currency == 'RUR':
-                self.__salary = salary.get('from')
+                salary = salary.get('from')
             else:
-                self.__salary = Vacancy.convert_to_rub(currency, salary.get('from'))
-        elif isinstance(salary, (int, float)):
+                salary = Vacancy.convert_to_rub(currency, salary.get('from'))
+
+        if isinstance(salary, (int, float)):
             self.__salary = salary
         else:
             self.__salary = 0
@@ -82,12 +134,5 @@ class Vacancy:
             area = item.get('area').get('name')
             employer = item.get('employer').get('name')
             vacancy = Vacancy(title, url, salary, requirements, area, employer)
-            Vacancy.all.append({
-                'title': vacancy.title,
-                'url': vacancy.url,
-                'salary': vacancy.salary,
-                'requirements': vacancy.requirements,
-                'area': vacancy.area,
-                'employer': vacancy.employer
-            })
+            Vacancy.all.append(vacancy)
         return Vacancy.all
